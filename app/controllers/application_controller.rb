62a06e9d38859073b68492fb55bb4cdee9b168c2
miyatrunteq <miyatrunteq@gmail.com>
2025-06-02 22:50:49 +0900

    Fix:ユーザー登録項目に名前を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :configure_permitted_parameters, if: :devise_controller? 
   allow_browser versions: :modern
     def after_sign_in_path_for(resource)
       homes_top_path
+    end
+      private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email,:name])
     end
 end
